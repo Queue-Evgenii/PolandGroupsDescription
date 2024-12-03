c373a3f3a42298955e7359ea6f5bb363a7946718
Queue-Evgenii <genadolgii228@gmail.com>
2024-12-03 13:58:38 +0200

    router hash disabled

--- quasar.config.js
+++ quasar.config.js
@@ -44,10 +44,10 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: "hash", // available values: 'hash', 'history'
+      vueRouterMode: "history", // available values: 'hash', 'history'
       distDir: "dist",
       // transpile: false,
-      // publicPath: '/',
+      publicPath: "/",
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
